4382f640f9e552b058d06b2bb2c037ec5ce78dc0
Diego Banovaz <diego.banovaz@gmail.com>
2020-02-20 11:55:43 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client"
+import Moment from 'react-moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  socket = null;
+
+  state = {
+    messages: [],
+    messageText: "",
+    username: "",
+    password: "",
+    token: "",
+    to: "",
+    connectedUsers: []
+  }
+
+  componentDidMount = async () => {
+    this.socket = io('http://localhost:8080', {
+      transports: ['websocket']
+    })
+
+    this.socket.on("message", payload => {
+      this.setState({
+        messages: this.state.messages.concat(payload)
+      })
+    })
+
+    if (localStorage.getItem("token")) {
+      const resp = await fetch("http://localhost:8080/auth/refresh", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("token")
+        }
+      })
+      if (resp.ok){
+        const json = await resp.json();
+        this.setState({ token: json.access_token, username: json.user.username })
+        this.socket.emit("login", { token: localStorage.getItem("token")  })
+        const respMessages = await fetch("http://localhost:8080/message", {
+          headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token")
+          }
+        })
+        
+        const msgs = await respMessages.json()
+        this.setState({
+          messages: msgs
+        })
+
+      }
+      else{
+        localStorage.removeItem("token")
+      }
+    }
+
+    this.socket.on("login", (user) => {
+      console.log(user.newUser + " is connected")
+      this.setState({
+        connectedUsers: user.connectedUsers
+      })
+    })
+  }
+
+  sendMessage = () => {
+    const payload = {
+      message: this.state.messageText,
+      from: this.state.username,
+      to: this.state.to,
+      date: new Date()
+    }
+    this.socket.emit("message", payload)
+    this.setState({
+      messageText: "",
+      messages: this.state.messages.concat(payload)
+    })
+  }
+
+  login = async () => {
+    const resp = await fetch("http://localhost:8080/auth/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const json = await resp.json();
+    this.setState({ token: json.access_token })
+    localStorage.setItem("token", json.access_token)
+
+    this.socket.emit("login", { token: json.access_token })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input type="text" value={this.state.username} onChange={(e) => this.setState({ username: e.currentTarget.value })} />
+          <input type="password" value={this.state.password} onChange={(e) => this.setState({ password: e.currentTarget.value })} />
+          <input type="button" onClick={this.login} value="Login" />
+        </div>
+
+        <div>
+          {this.state.messages.map((m, i) => <div key={i}><Moment fromNow>{m.date}</Moment> - {m.from}: {m.message} </div>)}
+        </div>
+
+        <div className="chat-input">
+          <input type="text" value={this.state.messageText} onChange={(e) => this.setState({ messageText: e.currentTarget.value })} />
+          <select onChange={e => this.setState({ to: e.currentTarget.value })}>
+            {this.state.connectedUsers.map((user,i) => <option key={i} value={user}>{user}</option>)}
+          </select>
+          {/* <input type="text" placeholder="to" value={this.state.to} onChange={(e) => this.setState({ to: e.currentTarget.value })} /> */}
+          <input type="button" onClick={this.sendMessage} value="send" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
